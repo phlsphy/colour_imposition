325a37faf4d7b6bc71180f2cdd89af0c52a86bb7
phlsphy <phillyosophy@gmail.com>
2017-08-24 00:20:51 +0800

    IO requests and pagination done

--- col_imp.ex
+++ col_imp.ex
@@ -5,15 +5,20 @@ defmodule M do
         total_page = IO.gets("What is the total number of pages in the book? ") |> String.trim |> String.to_integer
         IO.puts "Page Extent: #{total_page}pp."
             
-        # add function to calculate if divisble by 4.
+        # add function to calculate if divisble by 4. TBD
 
-        # ask for (only) front matter page extent
+        # ask (only) for front matter page extent
         front_matter = IO.gets("How many pages of Front Matter? ") |> String.trim
         IO.puts "Front matter extent: #{front_matter}pp."
 
-        # create a list/array of pages
-        pagination = Range.new(1,total_page)
-        #IO.puts "Pagination: #{pagination}" does not work, because range no longer binary/string.
+        # create a list/array of pages, 
+        # prints using 'inspect' (array is integer; puts is for binary) -> (1..n),
+        # convert to list
+        pagination = Range.new(1,total_page) 
+        IO.inspect [pagination], charlists: :as_lists 
+
+        # prints all individual members of list
+        Enum.to_list(pagination)
 
     end    
 
