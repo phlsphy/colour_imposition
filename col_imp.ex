c7ae2a0ba40b0763a054f831178ae5bb4982ace6
phlsphy <phillyosophy@gmail.com>
2017-08-24 00:04:56 +0800

    IO requests done

--- col_imp.ex
+++ col_imp.ex
@@ -1,21 +1,20 @@
 defmodule M do
     def main do 
-        # ask for inputs
-        # ask for total page extent
-        total_page = IO.gets("What is the total number of pages in the book? ") |> String.trim
+        # inputs
+        # ask for total page extent, trims white spaces, convert to integer
+        total_page = IO.gets("What is the total number of pages in the book? ") |> String.trim |> String.to_integer
         IO.puts "Page Extent: #{total_page}pp."
-
+            
         # add function to calculate if divisble by 4.
 
         # ask for (only) front matter page extent
         front_matter = IO.gets("How many pages of Front Matter? ") |> String.trim
         IO.puts "Front matter extent: #{front_matter}pp."
+
+        # create a list/array of pages
+        pagination = Range.new(1,total_page)
+        #IO.puts "Pagination: #{pagination}" does not work, because range no longer binary/string.
+
     end    
 
-    def array do 
-        # create the list/array of pages
-        # pagination = new(1, 12) | function "new" doesn't work - error: undefined
-        # try Enum next
-        IO.puts "Pages: #{pagination}"
-    end
 end
