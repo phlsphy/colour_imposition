64597d5e0bdbe7846db6aff9c9400b0dcd82c64b
phlsphy <phillyosophy@gmail.com>
2017-08-24 23:24:40 +0800

    Create blocks of 32

--- col_imp.ex
+++ col_imp.ex
@@ -2,24 +2,43 @@ defmodule M do
     def main do 
         # inputs
         # ask for total page extent, trims white spaces, convert to integer
-        total_page = IO.gets("What is the total number of pages in the book? ") |> String.trim |> String.to_integer
+        total_page = IO.gets("What is the total number of pages in the book? ") 
+            |> String.trim 
+            |> String.to_integer
         IO.puts "Page Extent: #{total_page}pp."
-            
-        # add function to calculate if divisble by 4. TBD
+        
+        # add function to calculate if divisble by 4. Just to make sure.  TBD
 
         # ask (only) for front matter page extent
-        front_matter = IO.gets("How many pages of Front Matter? ") |> String.trim
-        IO.puts "Front matter extent: #{front_matter}pp."
+        fm = IO.gets("How many pages of Front Matter? ") 
+            |> String.trim
+            |> String.to_integer
+        IO.puts "Front matter extent: #{fm}pp."
 
-        # create a list/array of pages, 
-        # prints using 'inspect' (array is integer; puts is for binary) -> (1..n),
-        # convert to list
-        pagination = Range.new(1,total_page) 
+        # 
+        # concatenate,
+        # then paginate,
+        fm_pagi = Range.new(1,fm)
+            |>IO.inspect()
+        body_matter = total_page - fm #<<<<<the problem
+            |>Range.new(1,body_matter)
+            |>IO.inpsect()
+
+        pagination = fm_pagi ++ body_matter
+            |>Enum.chunk_every(32)
         IO.inspect [pagination], charlists: :as_lists 
-
+        
+        # -------This below part works -----
+        # create a list/array of pages, 
+        # split into blocks of 32
+        # print using 'inspect' (array is integer; puts is for binary) -> (1..n),
+        # convert to list 
+        #pagination = Range.new(1,total_page) 
+        #    |>Enum.chunk_every(32)
+        #IO.inspect [pagination], charlists: :as_lists 
+        
         # prints all individual members of list
-        Enum.to_list(pagination)
+        #Enum.to_list(pagination)
 
     end    
-
 end
