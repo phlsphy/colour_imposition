d9ce39b64269b61fedab07eb390fa0ff4e1c09fc
phlsphy <phillyosophy@gmail.com>
2017-08-25 22:50:27 +0800

    v1.0

--- col_imp.ex
+++ col_imp.ex
@@ -2,41 +2,33 @@ defmodule M do
     def main do 
         # inputs
         # ask for total page extent, trims white spaces, convert to integer
-        total_page = IO.gets("What is the total number of pages in the book? ") 
+        body = IO.gets("How many pages of body matter? ") 
             |> String.trim 
             |> String.to_integer
-        IO.puts "Page Extent: #{total_page}pp."
+        IO.puts "Page Extent (without Front Matter): #{body}pp."
         
-        # add function to calculate if divisble by 4. Just to make sure.  TBD
-
         # ask (only) for front matter page extent
         fm = IO.gets("How many pages of Front Matter? ") 
             |> String.trim
             |> String.to_integer
         IO.puts "Front matter extent: #{fm}pp."
 
-        # 
-        # concatenate,
-        # then paginate,
+        # paginate both fm and bm
         fm_pagi = Range.new(1,fm)
             |>IO.inspect()
-        body_matter = total_page - fm #<<<<<the problem
-            |>Range.new(1,body_matter)
-            |>IO.inpsect()
 
-        pagination = fm_pagi ++ body_matter
-            |>Enum.chunk_every(32)
-        IO.inspect [pagination], charlists: :as_lists 
-        
-        # -------This below part works -----
-        # create a list/array of pages, 
+        body_pagi = Range.new(1,body) 
+            |>IO.inspect()
+
+        # concatenate the two lists. [] are crucial. They are lists!
         # split into blocks of 32
+        # | or ++ does not work, creates 2 lists together, cannot chunk.
+        Enum.concat([fm_pagi,body_pagi])
+            |>Enum.chunk_every(32)
+
         # print using 'inspect' (array is integer; puts is for binary) -> (1..n),
         # convert to list 
-        #pagination = Range.new(1,total_page) 
-        #    |>Enum.chunk_every(32)
-        #IO.inspect [pagination], charlists: :as_lists 
-        
+        #IO.inspect [pagination], chatlists: :as_lists
         # prints all individual members of list
         #Enum.to_list(pagination)
 
