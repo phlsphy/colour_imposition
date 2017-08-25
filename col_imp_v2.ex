bdfb2c857589acbf1b656c67085e260a17253d7f
phlsphy <phillyosophy@gmail.com>
2017-08-25 23:34:56 +0800

    v2.0

---
+++ col_imp_v2.ex
@@ -0,0 +1,20 @@
+defmodule M do
+    
+    IO.puts "Please enter 'M.pagi(a,b)' where 'a' is the total pages of Front Matter and 'b' is the total pages of the book (without Front Matter)."
+
+    def pagi(fm,body) do 
+        # Main pupose of col_imp_v2.ex is to see if we can code a more elegant/shorter solution
+        # As far as possible, v2 uses the same variable naming as v1
+
+        fm_pagi = Range.new(1,fm)
+            |>IO.inspect()
+
+        body_pagi = Range.new(1,body) 
+            |>IO.inspect()
+
+        pagination = Enum.concat([fm_pagi,body_pagi])
+            |>Enum.chunk_every(32)
+        IO.inspect [pagination], charlists: :as_lists
+
+    end    
+end
